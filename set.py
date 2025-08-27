20cf5c8227b7bff641e35807b27969f9836765d3
Simik Nainwal <simikjio@gmail.com>
2025-08-27 19:43:36 +0530

    Tuple and Sets

---
+++ set.py
@@ -0,0 +1,57 @@
+#***** Set *****
+#=>Sets are unordered and doesn't allow duplicate values.
+#=>Sets can only remove and add.
+
+# num={1,2,3,4,5,3,2}
+# print(num)
+# print(type(num))
+
+a={11,22,33,44,55,22}
+b={55,44,33,22,11}
+c={33,44,22,55,11}
+# print(a)
+# print(b)
+# print(c)
+#We can elements in different arrangement because the elements are stores in hash key form
+#For repeated values hash keys is same so we don't get duplicate values.
+#We can't apply slicing
+s={11,22,33,44,55}
+# for i in s:
+    # print(i)
+    
+    
+#****** Set Methods ******
+#s.add(x)
+# s.update(t)=>To join two sets
+# s.clear()
+# s.remove(x)
+# s.discard(x)  Check the difference between remove and discard.
+
+#The below methods gives output in true or false
+# s1.issuperset(s2)
+# s2.superset(s1)
+# s1.isdisjoint(s2)
+
+#union()
+#intersection()
+#difference()
+#symmetric_difference()
+
+num={1,2,3,4,5}
+num1={6,7,8,9}
+# num.update(num1)#In the argument we are passing a set
+# num.add(3) # in argument we are passing a number
+# num.clear()
+# num.remove(6) #gives error if value is not in the set
+num.discard(6)#doesn't give the error if value is not in the set
+# print(num)# the value gets overwrited.
+
+
+batsmen={"Shubhman Gill","Kohli","Dhoni","Rohit","Hardik","Jadeja"}
+bowlers={"Siraj","Hardik","Jasprit","Krishna","Jadeja"}
+print(batsmen.union(bowlers))
+print(batsmen.intersection(bowlers))
+print(batsmen.difference(bowlers))
+print(batsmen.symmetric_difference(bowlers))
+# print(batsmen & bowler) Also does the intersection operation
+
---
+++ tuple.py
@@ -0,0 +1,89 @@
+# ***** Tuple *****
+#=>Tuples are ordered and immutable or they can't be modified.
+#=>wriiten in () round brackets.
+#=>Allow duplicate elements.
+num=(1,2,3,4,5)
+# print(num)
+# print(type(num))
+
+t=()#Empty tuple
+# print(t)
+
+num1=(10)#Python interpreter considers it as a integer value
+num2=(10,)#It considers it as tuple
+# print(num)
+# print(type(num1))
+# print(type(num2))
+
+num=('A',)
+# print(type(num))
+
+num=(1,2,3,4,5)
+# print(num[::-1])# In the backend starting index is (length of variable) -1  
+# 5>-1; 5--
+
+# for i in num:
+#     print(i)
+
+# for i in range(0,len(num)):
+#     print(i,num[i])
+
+fruits=("Apple","Cherry","Kiwi","Berry")
+# fruits[2]="Orange"#error
+#To insert we have to do wrkaround=> first we convert tuple into list and then back into tuple.
+fruitsl=list(fruits)
+fruitsl.insert(2,"Orange")
+fruitst=tuple(fruitsl)
+# print(fruitst)
+
+#Consider the given tuple cities=("Mumbai","Dehradun","Delhi","Bangalore","Chennai","Raipur").Find the index of city "Dehradun" and remove it using pop() and print the remaining values in the tuple
+
+cities=("Mumbai","Dehradun","Delhi","Bangalore","Chennai","Raipur")
+citiesl=list(cities)
+# for i in range(0,len(citiesl)):
+#     if citiesl[i]=="Dehradun":
+#         x=i
+x=citiesl.index("Dehradun")#To find the index of the word.
+citiesl.pop(x)
+cities=tuple(citiesl)
+# print(cities)        
+
+# num=(1,2,1,4)
+# print(num)
+
+x=(1,2,3)
+y=(4,5,*x,6)#'*' unpacks the values of x
+# print(y)
+
+numd=(
+    (1,2,3),
+    (4,5,6),
+    (7,8,9)
+)
+# print(numd[0])#prints the whole row
+# print(numd[0][2])
+
+#Also valid in list
+# numdl=[
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9]
+# ]
+# print(numdl)
+
+# input = (1,2,3), element =4
+#output = (1,2,3,4)
+
+num=()
+numl=list(num)
+x=(int)(input())
+for i in range(0,x):
+    e=(int)(input())
+    # numl.insert(i,e)
+    numl.append(e)
+print(numl)
+n=(int)(input())
+# numl=list(num)
+numl.append(n)
+num=tuple(numl)
+print(num)
